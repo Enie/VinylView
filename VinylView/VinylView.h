cb40198d9a46ca15d1d2a4b71451da96a2a38a04
Enie Weiß <weiss.enie@gmail.com>
2022-10-01 12:12:55 +0200

    Initial Commit

---
+++ VinylView/VinylView.h
@@ -0,0 +1,18 @@
+//
+//  VinylView.h
+//  VinylView
+//
+//  Created by Enie Weiß on 01.10.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VinylView.
+FOUNDATION_EXPORT double VinylViewVersionNumber;
+
+//! Project version string for VinylView.
+FOUNDATION_EXPORT const unsigned char VinylViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VinylView/PublicHeader.h>
+
+
